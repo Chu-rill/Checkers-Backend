e9d3d3b1f57c9e19251cdb3422fee2430727123d
Chu-rill <churchillrimamskep@gmail.com>
2024-05-24 21:30:03 +0100

    did the route

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const { options } = require("joi");
+const port = process.env.PORT || 4006;
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+    res.json({ status: "success request" });
+});
+app.listen(port, () => console.log(`Server started on port ${port}`));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+// const authRoutes = require("./src/routes/AuthRoutes");
+// const taskRoutes = require("./src/routes/TaskRoutes");
+const { options } = require("joi");
+const port = process.env.PORT || 4006;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+//import your routes
+app.get("/", (req: any, res: any) => {
+  res.json({ status: "success request" });
+});
+
+// app.use("/task", taskRoutes);
+// app.use("/auth", authRoutes);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
