ef46e69e50be426b5cefbe1d3eeeda20eee6dae0
Chu-rill <churchillrimamskep@gmail.com>
2024-05-26 23:24:06 +0100

    changed

--- src/UserService.ts
+++ src/UserService.ts
@@ -1,21 +1,26 @@
-const connection: any = require("db");
+import { Pool, RowDataPacket } from "mysql2/promise";
+
+const connection: Pool = require("db");
 
 interface User {
   id: number;
   email: string;
   username: string;
   password: string;
-  // Add any other fields present in the users table
 }
 
-exports.getUserByEmail = async (email: string) => {
-  const [rows] = await connection.execute(
+// exports.getUserByEmail = async (email: string) => {
+//   const [rows] = await connection.execute(
+//     "SELECT * FROM users WHERE email = ? LIMIT 1",
+//     [email]
+//   );
+exports.getUserByEmail = async (email: string): Promise<User | null> => {
+  const [rows] = await connection.execute<RowDataPacket[]>(
     "SELECT * FROM users WHERE email = ? LIMIT 1",
     [email]
   );
 
-  const user = rows[0] as User | undefined;
+  const user = rows[0] as User;
 
   return user;
 };
-// console.log("hello")
